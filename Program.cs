b82119e89205061b66b7ce0d1a9b303d66d181d8
RuanMSantos <ruancauancauan@gmail.com>
2024-08-13 22:38:52 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿for (int n = 0; n <= 10; n++)
+{
+    #pragma warning disable CA1416
+    Thread.Sleep(500);
+    Console.Write($"{n} ");
+    Console.Beep(5000, 500);
+    #pragma warning restore CA1416
+}
